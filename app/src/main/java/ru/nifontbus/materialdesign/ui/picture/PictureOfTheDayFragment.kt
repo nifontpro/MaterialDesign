895b5aace1a62a0a4c4270742619af8e1ca5d003
Nifont <nifontbus@yandex.ru>
2021-06-29 14:16:03 +0300

    1.2 Начальная загрузка фото NASA

---
+++ app/src/main/java/ru/nifontbus/materialdesign/ui/picture/PictureOfTheDayFragment.kt
@@ -0,0 +1,91 @@
+package ru.nifontbus.materialdesign.ui.picture
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import android.view.Gravity
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import coil.api.load
+import ru.nifontbus.materialdesign.R
+import ru.nifontbus.materialdesign.databinding.MainFragmentBinding
+
+class PictureOfTheDayFragment : Fragment() {
+
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: PictureOfTheDayViewModel by lazy {
+        ViewModelProvider(this).get(PictureOfTheDayViewModel::class.java)
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getData()
+            .observe(viewLifecycleOwner, { renderData(it) })
+    }
+
+    private fun renderData(data: PictureOfTheDayData) {
+        when (data) {
+            is PictureOfTheDayData.Success -> {
+                val serverResponseData = data.serverResponseData
+                val url = serverResponseData.url
+                if (url.isNullOrEmpty()) {
+                    //Отобразите ошибку
+                    //showError("Сообщение, что ссылка пустая")
+                } else {
+                    //Отобразите фото
+                    //showSuccess()
+                    //Coil в работе: достаточно вызвать у нашего ImageView
+                    //нужную extension-функцию и передать ссылку и заглушки для placeholder
+                    binding.imageView.load(url) {
+                        lifecycle(this@PictureOfTheDayFragment)
+                        error(R.drawable.ic_load_error_vector)
+                        placeholder(R.drawable.ic_no_photo_vector)
+                    }
+                }
+            }
+
+            is PictureOfTheDayData.Loading -> {
+                showLoading()
+            }
+            is PictureOfTheDayData.Error -> {
+                toast(data.error.message)
+            }
+        }
+    }
+
+    private fun showLoading() {
+        TODO("Not yet implemented")
+    }
+
+    private fun Fragment.toast(string: String?) {
+        Toast.makeText(context, string, Toast.LENGTH_SHORT).apply {
+            setGravity(Gravity.BOTTOM, 0, 250)
+            show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        fun newInstance() = PictureOfTheDayFragment()
+    }
+}
--- app/src/main/java/ru/nifontbus/materialdesign/util/EquilateralImageView.kt
+++ app/src/main/java/ru/nifontbus/materialdesign/util/EquilateralImageView.kt
@@ -1,4 +1,16 @@
 package ru.nifontbus.materialdesign.util
 
-class EquilateralImageView {
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class EquilateralImageView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatImageView(context, attrs, defStyleAttr) {
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+    }
 }
