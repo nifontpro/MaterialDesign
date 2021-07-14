2705561ef2ce3bd8c40cb9bd0474ca576fcd2f8a
Nifont <nifontbus@yandex.ru>
2021-07-14 23:36:21 +0300

    5.4 Аннимация по клику на картинке

--- app/src/main/java/ru/nifontbus/materialdesign/ui/bottom/BottomNavigationDrawerFragment.kt
+++ app/src/main/java/ru/nifontbus/materialdesign/ui/bottom/BottomNavigationDrawerFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import ru.nifontbus.materialdesign.R
 import ru.nifontbus.materialdesign.databinding.BottomNavigationLayoutBinding
+import ru.nifontbus.materialdesign.ui.animations.one.AnimationsFragment
 import ru.nifontbus.materialdesign.ui.animations.one.AnimationsFragmentImage
 import ru.nifontbus.materialdesign.ui.animations.two.AnimationsFragmentBonus
 import ru.nifontbus.materialdesign.ui.picture.replaceFragment
@@ -29,7 +30,7 @@ class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.navigationView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.navigation_one -> replaceFragment(AnimationsFragmentImage())
+                R.id.navigation_one -> replaceFragment(AnimationsFragment())
                 R.id.navigation_two -> replaceFragment(AnimationsFragmentBonus())
             }
             dismiss()
