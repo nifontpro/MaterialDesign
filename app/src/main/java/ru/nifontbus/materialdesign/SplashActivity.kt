1dc13c3ae0ac69e603b742110eca8d4680e2d4e2
Nifont <nifontbus@yandex.ru>
2021-07-23 22:55:47 +0300

    8.2 Добавил аннимацию на заставку

--- app/src/main/java/ru/nifontbus/materialdesign/SplashActivity.kt
+++ app/src/main/java/ru/nifontbus/materialdesign/SplashActivity.kt
@@ -1,23 +1,59 @@
 package ru.nifontbus.materialdesign
 
+import android.animation.Animator
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.LinearInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import ru.nifontbus.materialdesign.databinding.ActivitySplashBinding
 
 var handler = Handler(Looper.getMainLooper())
 
 class SplashActivity : AppCompatActivity() {
 
+    private var _binding: ActivitySplashBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        _binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+/*        binding.imageView.animate().rotationBy(750f)
+            .setInterpolator(LinearInterpolator()).duration = 10000
+
 
         handler.postDelayed({
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             finish()
         }, 3000)
+        */
+
+        binding.imageView.animate().rotationBy(750f)
+            .setInterpolator(LinearInterpolator()).setDuration(2000)
+            .setListener(object : Animator.AnimatorListener {
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }
+
+                override fun onAnimationRepeat(animation: Animator?) {}
+                override fun onAnimationCancel(animation: Animator?) {}
+                override fun onAnimationStart(animation: Animator?) {}
+            })
+
+        // Вращение вокруг оси оХ
+/*        binding.imageView.animate().scaleY(-1f)
+            .setInterpolator(AccelerateDecelerateInterpolator()).duration = 1000
+
+        handler.postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 1200)*/
+
     }
 
     override fun onDestroy() {
